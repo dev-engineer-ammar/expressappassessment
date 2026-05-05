0543ca10939e5066343c11e9033d331f37e0d314
muhammadammarjaved <ammar.javid@narsunstudios.com>
2026-05-06 01:28:17 +0500

    unprotect user remove

--- models/Timesheet.js
+++ models/Timesheet.js
@@ -1,8 +1,24 @@
+// const mongoose = require("mongoose");
+
+// const timesheetSchema = new mongoose.Schema(
+//   {
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     week: { type: Number, required: true },
+//     dateRange: { type: String, required: true },
+//     status: {
+//       type: String,
+//       enum: ["COMPLETED", "INCOMPLETE", "MISSING"],
+//       default: "INCOMPLETE",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Timesheet", timesheetSchema);
 const mongoose = require("mongoose");
 
 const timesheetSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     week: { type: Number, required: true },
     dateRange: { type: String, required: true },
     status: {
